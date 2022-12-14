a89b63f5b44d5e378fc5210dcce00ce2292341de
Francesco <cesaranofrancescomain@gmail.com>
2022-12-14 16:24:02 +0100

    cambio impostazione esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script >
-import Head from './components/Head.vue'
 
-export default {
-  components: {
-    Head
-  }
-
-}
 </script>
 
 <template>
 
-  <Head />
+
 
 
 </template>
 
 <style lang="scss">
-@use "../src/styles/generals.scss" as *;
+@use "../src/styles/generals.scss";
+
+
+main {
+  padding-top: 20px;
+}
 </style>
