0cf07317a1cb7aa8a9fd9438abc293786a71627a
Francesco <cesaranofrancescomain@gmail.com>
2022-12-14 16:44:04 +0100

    importati store e appHeader  con bind sul comp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script >
+import AppHeader from './components/AppHeader.vue'
+
+
+import { store } from './store.js'
+
+export default {
+
+  name: "App",
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store,
+    }
+  }
+}
 
 </script>
 
+
 <template>
-
-
-
-
+  <AppHeader :msg="store.titolo" />
 </template>
 
 <style lang="scss">
