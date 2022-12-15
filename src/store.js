d024756ed82d6a551d25a3b537e1c6a770f080ae
Francesco <cesaranofrancescomain@gmail.com>
2022-12-15 15:29:36 +0100

    Aggiunte opzioni status e parameter

--- src/store.js
+++ src/store.js
@@ -4,5 +4,11 @@ export const store = reactive({
     characterList: [],
     apiURL: "https://rickandmortyapi.com/api/character",
     titolo: "Rick and Morty App",
-
+    status: "Open this select menu",
+    parameter: "status",
+    options: [
+        { text: 'Alive', value: 'Alive' },
+        { text: 'Dead', value: 'Dead' },
+        { text: 'Unknown', value: 'unknown' },
+    ]
 });
